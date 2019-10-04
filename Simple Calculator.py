8613806d7ae04802a2574e07e974728334bbadcc
Shahed Mohammad Hridoy <35490924+ShahedMohammadHridoy@users.noreply.github.com>
2019-10-04 11:29:09 +0600

    Square Root Added

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,11 +1,14 @@
 #author: Shahed Mohammad Hridoy
+from math import sqrt
 while True:
       print("Options:")
       print("Enter 'add' to add two numbers")
       print("Enter 'subtract' to subtract two numbers")
       print("Enter 'multiply' to multiply two numbers")
       print("Enter 'divide' to divide two numbers")
+      print("Enter 'sqrt' to square root a number")
       print("Enter 'quit' to end the program")
+      
 
       user_input = input(": ")
       
@@ -27,5 +30,8 @@ while True:
             a = float(input("Enter first number: "))
             b = float(input("Enter second number: "))
             print(a/b)
+      elif user_input == "sqrt":
+            a = float(input("Enter a number to root"))
+            print(sqrt(a))
       else:
             print ("invalid Input")
