eb01a0780b08ab62350da8c19c5f272a94a5cb08
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2019-10-29 17:22:55 +0600

    sum of five numbers added

---
+++ sum_of_five_numbers.py
@@ -0,0 +1,9 @@
+a = int(input("Enter value for a: "))
+b = int(input("Enter value for b: "))
+c = int(input("Enter value for c: "))
+d = int(input("Enter value for d: "))
+e = int(input("Enter value for e: "))
+
+sum = a + b + c + d + e
+
+print(sum)
