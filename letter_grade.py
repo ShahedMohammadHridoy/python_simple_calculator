057a8ff2cf377220f733d6eac4488c3375747055
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2019-11-10 19:56:42 +0600

    letter grade

---
+++ letter_grade.py
@@ -0,0 +1,15 @@
+marks = int(input("Input Marks: "))
+if marks>=80:
+    print("A+")
+elif marks>=70:
+    print("A")
+elif marks>=60:
+    print("A-")
+elif marks>=50:
+    print("B")
+elif marks>=40:
+    print("C")
+elif marks>32:
+    print("D")
+else:
+    print("F")
