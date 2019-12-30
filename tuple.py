bd21a724fb5985352014fe809024325a7afe8d4e
Shahed Mohammad Hridoy <35490924+ShahedMohammadHridoy@users.noreply.github.com>
2019-12-30 17:06:39 +0600

    tuple

---
+++ tuple.py
@@ -0,0 +1,20 @@
+# Emplty Tuple
+tup = ()
+
+# Tuple having integers
+int_tup = (1,2,3,4,5,6,7)
+
+# Tuple having Strings
+str_tup = ("a","e","i","o","u")
+
+# Tuple with mixed datatypes
+mixed_tup = (1,2,3,4,5,6,"python",3.14)
+
+# Tuple of strings and list
+new_tup = ("python", [1,2,3,4,5,6])
+
+# Tuple inside another tuple / nested tuple
+nested_tup = ((1,2,3,4),(5,6,7))
+
+# Tuple with single element
+tup2 = (1,)
